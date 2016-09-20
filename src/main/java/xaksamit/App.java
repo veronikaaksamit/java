f2b17afd531e959ad63f6c6c8c46838018e6696c
Veronika Aksamitova <xaksamit@nymfe39.fi.muni.cz>
2016-09-20 17:17:16 +0200

    Hello PA165

---
+++ src/main/java/xaksamit/App.java
@@ -0,0 +1,14 @@
+package xaksamit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA195" );
+    }
+}
+
